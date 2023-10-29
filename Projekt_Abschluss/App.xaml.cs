662c478e32092dd1c881877871624f841d802aaa
Elisama osuna vargas <k4in16@gmail.com>
2023-10-29 19:14:42 +0100

    estilos y pollas

--- Projekt_Abschluss/App.xaml.cs
+++ Projekt_Abschluss/App.xaml.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
 namespace Projekt_Abschluss
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+    
     public partial class App : Application
     {
+        public App()
+        {
+            CultureInfo cultureInfo = new CultureInfo("en-US");
+            CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+            CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+        }
     }
 }
